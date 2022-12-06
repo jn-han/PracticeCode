ae115ca898b47403b434ec881277a0dac4a539ef
jn-han <joshuahnguyen@gmail.com>
2022-12-05 20:53:46 -0800

    added names to buttons

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,10 @@ import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
 
+    const clickHandler = () => {
+        console.log("clicked!");
+    }
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>
@@ -11,7 +15,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button></button>
+            <button onClick={clickHandler}>Change Title</button>
         </Card>
     )
 }
--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -11,9 +11,17 @@ function Expenses(props) {
                 amount={props.items[0].amount}
                 date={props.items[0].date}
             />
+            <ExpenseItem 
+                title={props.items[1].title}
+                amount={props.items[1].amount}
+                date={props.items[1].date}
+            />
+            <ExpenseItem 
+                title={props.items[2].title}
+                amount={props.items[2].amount}
+                date={props.items[2].date}
+            />
         </Card>
-
     )
 }
-
 export default Expenses;
