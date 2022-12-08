f3bcb7d7b663f8e123e136ce3d58f248a7728d64
jn-han <joshuahnguyen@gmail.com>
2022-12-07 20:23:21 -0800

    npm react scripts installation

--- 01-starting-setup/src/components/NewExpense/NewExpense.js
+++ 01-starting-setup/src/components/NewExpense/NewExpense.js
@@ -2,8 +2,17 @@ import './NewExpense.css'
 import ExpenseForm from './ExpenseForm';
 
 const NewExpense = () => {
+
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString(),
+        }
+        console.lof(expenseData);
+    }
+
     return <div className='new-expense'>
-        <ExpenseForm/>
+        <ExpenseForm onSaveExpenseData />
     </div>
 
 
