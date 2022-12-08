baa6bcf6f21c9a94c833fe47eaf7c70cb5ced6df
jn-han <joshuahnguyen@gmail.com>
2022-12-07 17:25:26 -0800

    Added functional buttons to React shopping list project

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,25 @@
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+import React, {useState} from 'react';
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
 
     const clickHandler = () => {
-        console.log("clicked!");
+        setTitle('hello');
+        console.log("click!!")
     }
 
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
             <button onClick={clickHandler}>Change Title</button>
-            <h1>hello</h1>
         </Card>
-        
     )
 }
 
