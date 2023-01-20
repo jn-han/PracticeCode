c93a7fe0d5dc66d8baaa452d05535b91a75ea358
jn-han <joshuahnguyen@gmail.com>
2023-01-19 16:38:38 -0800

    update 01-starting-setup

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -39,7 +39,6 @@ function App() {
   
   return (
     <div>
-      <h1>{DUMMY_EXPENSES[1].date.toDateString()}</h1>
       <NewExpense ExpenseDataHandler={ExpenseDataHandler}/>
       <Expenses items={expenses}/>
     </div>
